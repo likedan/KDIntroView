f3a9ba5656c610b550eb76915e7d1714cbaf776b
likedan <likedan5@icloud.com>
2015-07-05 16:24:30 -0500

    push

--- Example/KDIntroView/ViewController.swift
+++ Example/KDIntroView/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import KDIntroView
 
 class ViewController: UIViewController {
 
+    //var ss = kdi
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
