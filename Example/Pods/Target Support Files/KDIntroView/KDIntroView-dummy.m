fad4518037752e05c75ec7c110fad0f6536379bf
likedan <likedan5@icloud.com>
2015-07-05 16:35:38 -0500

    delete old example

--- Example/Pods/Target Support Files/KDIntroView/KDIntroView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KDIntroView : NSObject
-@end
-@implementation PodsDummy_KDIntroView
-@end
--- Example/Pods/Target Support Files/Pods-KDIntroView_Example/Pods-KDIntroView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KDIntroView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_KDIntroView_Example
-@end
